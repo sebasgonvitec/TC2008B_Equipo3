c46a48a518754a14aa947926b501e4124b8c4e92
sebasgonvitec <a01029746@itesm.mx>
2022-11-15 14:06:11 -0600

    Added server and model python files

---
+++ actividad_robots/Server/RobotAgents.py
@@ -0,0 +1,10 @@
+"""
+Modelos de Agente y ambiente (alamacen)
+Movimiento de robots en el grid
+15-11-2022
+
+Autores: Sebastián González,
+         Andreína Sanánez,
+         Karla Mondragón,
+         Ana Paula Katsuda
+"""
---
+++ actividad_robots/Server/server.py
@@ -0,0 +1,9 @@
+"""
+Python Flask server for connection with Unity
+15-11-2022
+
+Autores: Sebastián González,
+         Andreína Sanánez,
+         Karla Mondragón,
+         Ana Paula Katsuda
+"""
