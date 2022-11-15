1f33ff0944c0cf4b3738df82ffbd29fc6a45c92e
sebasgonvitec <a01029746@itesm.mx>
2022-11-15 14:57:32 -0600

    Added base Unity Project and Server

--- actividad_robots/Server/server.py
+++ actividad_robots/Server/server.py
@@ -7,3 +7,62 @@ Autores: Sebastián González,
          Karla Mondragón,
          Ana Paula Katsuda
 """
+
+from flask import Flask, request, jsonify
+from RobotAgents import *
+
+# Size of the board:
+number_agents = 10
+width = 28
+height = 28
+randomModel = None
+currentStep = 0
+
+app = Flask("Traffic example")
+
+# @app.route('/', methods=['POST', 'GET'])
+
+@app.route('/init', methods=['POST', 'GET'])
+def initModel():
+    global currentStep, randomModel, number_agents, width, height
+
+    if request.method == 'POST':
+        number_agents = int(request.form.get('NAgents'))
+        width = int(request.form.get('width'))
+        height = int(request.form.get('height'))
+        currentStep = 0
+
+        print(request.form)
+        print(number_agents, width, height)
+        randomModel = RandomModel(number_agents, width, height)
+
+        return jsonify({"message":"Parameters recieved, model initiated."})
+
+@app.route('/getAgents', methods=['GET'])
+def getAgents():
+    global randomModel
+
+    if request.method == 'GET':
+        agentPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for (a, x, z) in randomModel.grid.coord_iter() if isinstance(a, RandomAgent)]
+
+        return jsonify({'positions':agentPositions})
+
+@app.route('/getObstacles', methods=['GET'])
+def getObstacles():
+    global randomModel
+
+    if request.method == 'GET':
+        carPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for (a, x, z) in randomModel.grid.coord_iter() if isinstance(a, ObstacleAgent)]
+
+        return jsonify({'positions':carPositions})
+
+@app.route('/update', methods=['GET'])
+def updateModel():
+    global currentStep, randomModel
+    if request.method == 'GET':
+        randomModel.step()
+        currentStep += 1
+        return jsonify({'message':f'Model updated to step {currentStep}.', 'currentStep':currentStep})
+
+if __name__=='__main__':
+    app.run(host="localhost", port=8585, debug=True)
