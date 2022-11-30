f9986fd7d363d038625beca2bcf712ba9494ebbe
asananez2000 <asananez2000@gmail.com>
2022-11-30 11:26:52 -0600

    Add Unity Design

--- actividad_robots/ActividadRobotsUnity/Assets/Scripts/CityMaker.cs
+++ actividad_robots/ActividadRobotsUnity/Assets/Scripts/CityMaker.cs
@@ -6,9 +6,19 @@ public class CityMaker : MonoBehaviour
 {
     [SerializeField] TextAsset layout;
     [SerializeField] GameObject roadPrefab;
-    [SerializeField] GameObject buildingPrefab;
+
+    [SerializeField] GameObject turnPrefab1;
+    [SerializeField] GameObject turnPrefab2;
+    [SerializeField] GameObject turnPrefab3;
+    [SerializeField] GameObject turnPrefab4;
+
+    [SerializeField] GameObject interPrefab;
+    public List<GameObject> buildingList;
+    [SerializeField] GameObject destination;
     [SerializeField] GameObject semaphorePrefab;
     [SerializeField] int tileSize = 1;
+
+    GameObject buildingPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -62,15 +72,59 @@ public class CityMaker : MonoBehaviour
                 x += 1;
             } else if (tiles[i] == 'D') {
                 position = new Vector3(x * tileSize, 0, y * tileSize);
-                tile = Instantiate(buildingPrefab, position, Quaternion.Euler(0, 90, 0));
+                tile = Instantiate(destination, position, Quaternion.Euler(0, 90, 0));
                 tile.transform.parent = transform;
                 x += 1;
             } else if (tiles[i] == '#') {
                 position = new Vector3(x * tileSize, 0, y * tileSize);
+                buildingPrefab = buildingList[Random.Range(0, 3)];
                 tile = Instantiate(buildingPrefab, position, Quaternion.identity);
+                tile.transform.localScale = new Vector3(1, Random.Range(1f, 1.5f), 1);
                 tile.transform.parent = transform;
                 x += 1;
-            } else if (tiles[i] == '\n') {
+            }  else if (tiles[i] == 'x') {
+                position = new Vector3(x * tileSize + 2, 0, y * tileSize - 2);
+                tile = Instantiate(interPrefab, position, Quaternion.Euler(0, 90, 0));
+                tile.transform.localScale = new Vector3(2, 1, 2);
+                tile.transform.parent = transform;
+                x += 1;
+            }
+            else if (tiles[i] == '&') {
+                position = new Vector3(x * tileSize + 2, 0, y * tileSize - 2);
+                buildingPrefab = buildingList[Random.Range(0, 7)];
+                tile = Instantiate(buildingPrefab, position, Quaternion.identity);
+                tile.transform.localScale = new Vector3(2, 2, 2);
+                tile.transform.parent = transform;
+                x += 1;
+            }
+            else if (tiles[i] == '1') {
+                position = new Vector3(x * tileSize, 0, y * tileSize);
+                tile = Instantiate(turnPrefab1, position, Quaternion.Euler(0, 90, 0));
+                tile.transform.parent = transform;
+                x += 1;
+            }
+            else if (tiles[i] == '2') {
+                position = new Vector3(x * tileSize, 0, y * tileSize);
+                tile = Instantiate(turnPrefab2, position, Quaternion.Euler(0, 90, 0));
+                tile.transform.parent = transform;
+                x += 1;
+            }
+            else if (tiles[i] == '3') {
+                position = new Vector3(x * tileSize, 0, y * tileSize);
+                tile = Instantiate(turnPrefab3, position, Quaternion.Euler(0, 90, 0));
+                tile.transform.parent = transform;
+                x += 1;
+            }
+            else if (tiles[i] == '4') {
+                position = new Vector3(x * tileSize, 0, y * tileSize);
+                tile = Instantiate(turnPrefab4, position, Quaternion.Euler(0, 90, 0));
+                tile.transform.parent = transform;
+                x += 1;
+            }
+            else if (tiles[i] == '.') {
+                x += 1;
+            }
+            else if (tiles[i] == '\n') {
                 x = 0;
                 y -= 1;
             }
