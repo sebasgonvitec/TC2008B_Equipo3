9002fdf40b23d313560a545350622cc574be5f8c
Akemi <akemi@Akemis-MacBook-Air.local>
2022-11-30 16:30:10 -0600

    Path detection bug fix

--- reto/Server/past_version/agent_graph.py
+++ reto/Server/past_version/agent_graph.py
@@ -17,7 +17,7 @@ class Car(Agent):
             destination: Coordinates of the destination
         """
         super().__init__(unique_id, model)
-        self.destination = (21,5)#destination
+        self.destination = destination
         self.route = None
         self.calculated_route = False
         self.moving = False
